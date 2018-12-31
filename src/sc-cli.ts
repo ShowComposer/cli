e97d4ac81b032fd5b23bdaa293445940712ae33c
Marvin Gaube <dev@marvingaube.de>
2018-12-31 15:34:09 +0100

    Fix version echo

--- src/sc-cli.ts
+++ src/sc-cli.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env node
+
 const pj = require('../package.json')
 import * as cli from 'commander';
 
-cli.version(pj.version);
+cli.version(pj.version, '-v, --version');
 
 cli.parse(process.argv);
