948658cf30db6d4fe33db61f6da1e9ff144f1da2
Marvin Gaube <dev@marvingaube.de>
2018-12-31 15:29:16 +0100

    Initial setup of commander

--- src/sc-cli.ts
+++ src/sc-cli.ts
@@ -1,3 +1,7 @@
 #!/usr/bin/env node
 const pj = require('./package.json')
-console.log("ShowComposer CLI version "+pj.version);
+import * as cli from 'commander';
+
+cli.version(pj.version);
+
+cli.parse(process.argv);
