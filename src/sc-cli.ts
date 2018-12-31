d4ce11f3e4f98dfafe40da3a58d0c337ad9d9c35
Marvin Gaube <dev@marvingaube.de>
2018-12-31 15:55:58 +0100

    Prepare tail cmd

--- src/sc-cli.ts
+++ src/sc-cli.ts
@@ -3,6 +3,12 @@
 const pj = require('../package.json')
 import * as cli from 'commander';
 
+// Version output
 cli.version(pj.version, '-v, --version');
+
+// Tail
+cli.command('tail [key]').description('streams all state changes and ticks').usage('[key], key is root if undefined').action((key,cmd) => {
+    console.log('Observing '+key);
+  })
 
 cli.parse(process.argv);
