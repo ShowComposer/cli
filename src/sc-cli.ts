2f17720a46eff189f7ade61892e45ffe978cd99b
Marvin Gaube <dev@marvingaube.de>
2018-12-31 16:01:34 +0100

    Add hostname option

--- src/sc-cli.ts
+++ src/sc-cli.ts
@@ -6,9 +6,12 @@ import * as cli from 'commander';
 // Version output
 cli.version(pj.version, '-v, --version');
 
+// Connection information
+cli.option('-H, --host', 'Host where sc-broker is running, defaults to localhost');
+
 // Tail
 cli.command('tail [key]').description('streams all state changes and ticks').usage('[key], key is root if undefined').action((key,cmd) => {
     console.log('Observing '+key);
-  })
+  });
 
 cli.parse(process.argv);
