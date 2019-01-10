572c7c33d503acfa35d1724afaaabbab89367b5c
Marvin Gaube <dev@marvingaube.de>
2019-01-10 20:27:25 +0100

    Add tslint

--- src/sc-cli.ts
+++ src/sc-cli.ts
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-const pj = require('../package.json')
-import * as cli from 'commander';
+const pj = require("../package.json");
+import * as cli from "commander";
 
 // Version output
-cli.version(pj.version, '-v, --version');
+cli.version(pj.version, "-v, --version");
 
 // Connection information
-cli.option('-H, --host', 'Host where sc-broker is running, defaults to localhost');
+cli.option("-H, --host", "Host where sc-broker is running, defaults to localhost");
 
 // Tail
-cli.command('tail [key]').description('streams all state changes and ticks').usage('[key], key is root if undefined').action((key,cmd) => {
-    console.log('Observing '+key);
+cli.command("tail [key]").description("streams all state changes and ticks").usage("[key], key is root if undefined").action((key, cmd) => {
+    console.log("Observing " + key);
   });
 
 cli.parse(process.argv);
